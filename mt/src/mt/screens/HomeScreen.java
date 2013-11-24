5af59fa5a0ede59e846375d824416aa75e208b46
zman2013 <vsmingren@qq.com>
2013-11-24 11:15:30 +0800

    add information in HomeButtonGroup of HomeScreen

--- mt/src/mt/screens/HomeScreen.java
+++ mt/src/mt/screens/HomeScreen.java
@@ -2,7 +2,7 @@ package mt.screens;
 
 import mt.actors.AbstractImage;
 import mt.actors.HeaderBox;
-import mt.actors.HomeButton;
+import mt.actors.HomeButtonGroup;
 import mt.actors.MenuBox;
 import mt.actors.TeamBox;
 import mt.resources.ResourcesLoader;
@@ -36,23 +36,15 @@ public class HomeScreen extends AbstractScreen{
 		teamBox.setScale( SCALE );
 		teamBox.setY( height - (180 + 325)*AbstractImage.SCALE );
 		
-		HomeButton heroButton = new HomeButton( ResourcesLoader.getHomeButtonDrawable(), ResourcesLoader.getHomeButtonActiveDrawable() );
-		heroButton.setPosition( width/2 - (410/2)*AbstractImage.SCALE, height - (180 + 325 + 70)*AbstractImage.SCALE );
-		HomeButton upgradeButton = new HomeButton( ResourcesLoader.getHomeButtonDrawable(), ResourcesLoader.getHomeButtonActiveDrawable() );
-		upgradeButton.setPosition( width/2 - (410/2)*AbstractImage.SCALE, height - (180 + 325 + 140)*AbstractImage.SCALE );
-		HomeButton revolutionButton = new HomeButton( ResourcesLoader.getHomeButtonDrawable(), ResourcesLoader.getHomeButtonActiveDrawable() );
-		revolutionButton.setPosition( width/2 - (410/2)*AbstractImage.SCALE, height - (180 + 325 + 210)*AbstractImage.SCALE );
-		HomeButton awardButton = new HomeButton( ResourcesLoader.getHomeButtonDrawable(), ResourcesLoader.getHomeButtonActiveDrawable() );
-		awardButton.setPosition( width/2 - (410/2)*AbstractImage.SCALE, height - (180 + 325 + 280)*AbstractImage.SCALE );
+		HomeButtonGroup homeButtonGroup = new HomeButtonGroup( );
+		homeButtonGroup.setScale( 0.7f );
+		homeButtonGroup.setPosition(  width/2f-90, 80 );
 		
 		MenuBox menuBox = new MenuBox( ResourcesLoader.getMenuButtonDrawable() );
 
 		stage.addActor( headerBox );
 		stage.addActor( teamBox );
-		stage.addActor( heroButton );
-		stage.addActor( upgradeButton );
-		stage.addActor( revolutionButton );
-		stage.addActor( awardButton );
+		stage.addActor( homeButtonGroup );
 		stage.addActor( menuBox );
 		
 	}
